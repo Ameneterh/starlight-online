7d13178f7a71c4251bbcc302dc1409bb24562ccd
ameneterh <ameneterh@gmail.com>
2022-11-25 23:54:39 +0100

    Added break points for smaller devices

---
+++ index.js
@@ -0,0 +1,6 @@
+var year = new Date().getFullYear();
+document.getElementById("copyrightYear").innerHTML = year;
+
+function clickMe(){
+  window.alert("Search Functionality not yet operational");
+}
